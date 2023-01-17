7eb8d3800999df5a4760904dc0605d6c36220390
ETIENNE HUGO <hugo.etienne@edu.univ-eiffel.fr>
2023-01-17 16:14:47 +0100

    TP2 Exo 2

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,12 +1,71 @@
 #include <iostream>
+#include <sstream>
+#include <set>
+#include <map>
+
+// Exo 1
+std::set<std::string> make_exit_commands()
+{
+    std::set<std::string> set;
+    set.emplace("q");
+    set.emplace("quit");
+    set.emplace("e");
+    set.emplace("exit");
+    return set;
+}
 
 int main()
 {
+    using namespace std;
+    string input;
+    string w1;
+    string w2;
+    set<string> set = make_exit_commands();
+    map<string, string>
+        map;
     while (true)
     {
-        std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::cin.ignore();
-    }
+        cin >> input;
+        if (set.count(input))
+        {
+            return 0;
+        }
+        if (input == "add")
+        {
+            cin >> w1;
+            cin >> w2;
+            map.emplace(w1, w2);
+            cout << w1 << " => " << w2 << endl;
+        }
+        if (input == "translate")
+        {
+            cin >> w1;
+            if (map.count(w1))
+            {
+                cout << map.find(w1)->second << endl;
+            }
+            else
+            {
+                cout << "???" << endl;
+            }
+        }
 
+        // stringstream inputStream(input);
+        // while (!inputStream.eof())
+        // {
+        //     inputStream >> word;
+        //     if (set.count(word))
+        //     {
+        //         return 0;
+        //     }
+        //     if (word == "add")
+        //     {
+        //         map.emplace(inputStream.get(), inputStream.get());
+        //     }
+        //     if (word == "translate")
+        //     {
+        //         map.find(inputStream.get());
+        //     }
+    }
     return 0;
 }
