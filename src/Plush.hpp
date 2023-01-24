633d7088ef611f0be000e72c10dd8dc3fae5c220
ETIENNE HUGO <hugo.etienne@edu.univ-eiffel.fr>
2023-01-24 16:03:34 +0100

    TP3 test-09

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -2,4 +2,26 @@
 
 class Plush
 {
+public:
+    Plush(){
+        _cost = 10;
+    }
+
+    Plush(const int &init)
+    {
+        _cost = init;
+    }
+
+    int get_cost() const
+    {
+        return _cost;
+    }
+
+    void set_cost(int new_cost)
+    {
+        _cost = new_cost;
+    }
+
+private:
+    int _cost;
 };
--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,47 @@
 #pragma once
+#include <string>
+
+class PlushStore
+{
+public:
+    PlushStore(const std::string &init){
+        _name = init;
+        _wealth = 0;
+        _stock = 0;
+    }
+
+    std::string get_name() const{
+        return _name;
+    }
+
+    int get_wealth_amount() const{
+        return _wealth;
+    }
+
+    int get_stock_size() const{
+        return _stock;
+    }
+
+    void loan(int amount){
+        _wealth += amount;
+    }
+
+    int get_debt_amount() const{
+        return _wealth + _wealth/10;
+    }
+
+    void make_plush(int cost){
+        if(_wealth != 0){
+            _wealth -= cost;
+            if(_wealth < 0){
+                _wealth = 0;
+            }
+            _stock += 1;
+        }
+    }
+
+private:
+    std::string _name;
+    int _wealth;
+    int _stock;
+};
